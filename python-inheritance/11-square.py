66de67172d1a5d6f3c7480de4dedc3289215f5c4
heritier <mytoken@gmail.com>
2023-07-25 09:53:17 -0700

    t11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
+"""
+class square
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
+"""
+Square class
+"""
+
+
 class Square(Rectangle):
-    """Represent a square."""
-
+    """ Square Class """
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+        """ instantiation with size """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        ''' implement area '''
+        return self.__size * self.__size
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
